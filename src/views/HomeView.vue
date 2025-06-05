7d0e17180cc70ea55d56117e0b80e7f303ac5377
JIAZILONG <15405888+JIA_ZILONG@user.noreply.gitee.com>
2025-06-06 00:47:52 +0800

    前端架构（空白页）+路由

---
+++ src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <h1>首页</h1>
+    <router-link to="/login"><button>去登录</button></router-link>
+    <router-link to="/register"><button>去注册</button></router-link>
+    <router-link to="/main"><button>主页面</button></router-link>
+  </div>
+</template>
---
+++ src/views/RegisterView.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h1>注册页</h1>
+    <router-link to="/"><button>回首页</button></router-link>
+    <router-link to="/login"><button>去登录</button></router-link>
+  </div>
+</template>
